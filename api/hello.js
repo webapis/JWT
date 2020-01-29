ade187da408799a8c4da4215f4d38e3bb9b59b40
webapis <webapis.github@gmail.com>
2020-01-29 17:12:13 +0500

    v

--- api/hello.js
+++ api/hello.js
@@ -10,5 +10,6 @@ module.exports = (req, res) => {
 
   console.log('__process.env.DB_NAME', process.env.DB_NAME);
   console.log('COLL_NAME', process.env.COLL_NAME);
+  console.log('MYY_VARIABLE', process.env.MYY_VARIABLE);
   res.status(200).send(`Hello ${name}!`);
 };
