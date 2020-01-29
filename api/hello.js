2107b8aee87491f21e715dad41085d2ed8ca5cd1
webapis <webapis.github@gmail.com>
2020-01-29 17:07:09 +0500

    coll

--- api/hello.js
+++ api/hello.js
@@ -1,13 +1,14 @@
 import getDbClient from './getDb';
 module.exports = (req, res) => {
   const { name = 'World' } = req.query;
-  try {
-    const client = getDbClient();
-    console.log('client', client);
-  } catch (error) {
-    console.log('client error', error);
-  }
+  // try {
+  //   const client = getDbClient();
+  //   console.log('client', client);
+  // } catch (error) {
+  //   console.log('client error', error);
+  // }
 
-  console.log('__process.env.DATABASE_NAME', process.env.YOUR_VALUE);
+  console.log('__process.env.DB_NAME', process.env.DB_NAME);
+  console.log('COLL_NAME', process.env.COLL_NAME);
   res.status(200).send(`Hello ${name}!`);
 };
