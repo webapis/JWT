9766f9d18505a5a8f5c33ab22646c59221eb3ea1
webapis <webapis.github@gmail.com>
2020-01-29 15:29:48 +0500

    v

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
   const { name = 'World' } = req.query;
   const url = process.env;
-  console.log('__process.env.DATABASE_NAME', process.env);
+  console.log('__process.env.DATABASE_NAME', process.env.YOUR_VALUE);
   res.status(200).send(`Hello ${name}!`);
 };
